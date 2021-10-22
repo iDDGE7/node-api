fe4fd149a9aa5ccdc69fd05e7ff7cd4da80317d6
Edgar de Dios <eduardoddg60@gmail.com>
2021-10-22 14:01:36 -0500

    port env added

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ App.get("/", (req, res) => {
   res.send(`<h1> Welcome </h1>`);
 });
 
-App.listen(3000, () => console.log("server on port 3000"));
+const port = process.env.PORT || 3000;
+App.listen(port, () => console.log("server on port 3000"));
